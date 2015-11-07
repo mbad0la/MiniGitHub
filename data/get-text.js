f5efec190157674f172ffb5deef13df368809efd
mbad0la <badola21295@gmail.com>
2015-11-07 04:11:17 -0800

    added some user info in addon

--- data/get-text.js
+++ data/get-text.js
@@ -1,4 +1,32 @@
-
+function loadstats(user)
+{
+    $('#wrapper').append('<div id="statwrap"></div>');
+    $.ajax({
+        url:'https://mbad0la.pythonanywhere.com/follower',
+        type:'post',
+        dataType:'json',
+        data:{u:user,p:"",inapp:1},
+        success:function(r1){
+            $('#statwrap').append('<div class="stats"><span class="count">'+r1.length+'</span><br>Follower</div>');
+            $.ajax({
+            url:'https://mbad0la.pythonanywhere.com/following',
+            type:'post',
+            dataType:'json',
+            data:{u:user,p:"",inapp:1},
+            success:function(r2){
+            $('#statwrap').append('<div class="stats"><span class="count">'+r2.length+'</span><br>Following</div>');
+            
+            $('#statwrap').fadeIn();
+            },
+            error:function(r2){console.log(r2);}
+            });
+        },
+        error:function(r1){console.log(r1);}
+        
+    });
+    
+    
+}
 $("#wrapper").on('click','#btn',function(){
       
     $.ajax({
@@ -8,13 +36,24 @@ dataType:'json',
 data:{u:$('#username').val(),p:$('#cred').val(),inapp:0},
 success:function(r){
     console.log(r['name'])
+    $('#wrapper').append('<img id="myimg" src="'+r['avatar_url']+'"/>');
+    
+    $('#wrapper').find('#myimg').load(function(){
+        $('#loader,#wall').fadeOut().remove();
+        $(this).fadeIn();
+        
+        loadstats(r['login']);
+        
+        });
     self.port.emit("authorised",r['login']);
     },
 error:function(r){console.log(r);}
 });
     
     $('#username,#cred,#btn').remove();
-    $('#wall').css('top',60);
+    $('#wrapper').append('<img id="loader" src="ajax-loader.gif"/>');
+    $('#wall').css('top',50);
+    //$('#wall').fadeOut(710);
         
     });
 
@@ -22,6 +61,8 @@ self.port.on("show", function onShow(user) {
     
     if (user.length==0) {
         $("#wrapper").empty().append('<img id="wall" src="github_icon.png"/><input type="text" id="username" placeholder="Username"/></br><input type="password" id="cred" placeholder="Password"/><div id="btn">Go!</div>');
+        
+        //$('#wrapper').append('<img id="wall" src="github_icon.png"/><div id="statwrap"><div class="stats"><span class="count">5</span><br>Follower</div><div class="stats"><span class="count">5</span><br>Following</div></div>');
     }
     else
     {
@@ -33,10 +74,19 @@ self.port.on("show", function onShow(user) {
         data:{u:user,p:"",inapp:1},
         success:function(r){
             console.log(r['name']);
+            $('#wrapper').append('<img id="myimg" src="'+r['avatar_url']+'"/>');
+    
+            $('#wrapper').find('#myimg').load(function(){
+            $('#loader').remove();
+            $(this).fadeIn();
+            loadstats(r['login']);
+            });
             self.port.emit("authorised",r['login']);
             },
         error:function(r){console.log(r);}
+        
 });
+        $('#wrapper').append('<img id="loader" src="ajax-loader.gif"/>');
     }
 
   
